399209b9c347099b0b02429939e4ad9cfd644dea
郷大助 <daisukegoh@guest1-95.csl.sony.co.jp>
2019-02-04 18:47:41 +0900

    completed

---
+++ src/components/Todo.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Todo extends Component {
+    render() {
+        return (
+            <li> A todo will go here </li>
+        );
+    }
+}
+
+export default Todo;
